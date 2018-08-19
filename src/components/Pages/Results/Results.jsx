f410b1743e4f1071149a44c3427f85a0116db284
Alexander Dyumin <AlekGit@yandex.ru>
2018-08-19 21:37:21 +0300

    add results page

--- src/components/Pages/Results/Results.jsx
+++ src/components/Pages/Results/Results.jsx
@@ -1,9 +1,46 @@
 import React, { Component, Fragment } from 'react';
+import { getResults } from '../../App/AppSelectors';
 import { connect } from 'react-redux';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
-export const Results = () => (
-  <div>
-    <h1>About Us</h1>
-    <p>Hello Medium!</p>
-  </div>
+export const Result = (result, index) => (
+  <ListItem key={ index }>
+    <ListItemText
+      primary={
+        `${result.from.symbol} ${result.from.value} === ${result.to.symbol} ${result.to.value}`
+      }
+    />
+  </ListItem>
 );
+
+class ResultsComponent extends Component {
+  render() {
+    return (
+      <Fragment>
+        <List subheader={ <li />}>
+          <ListSubheader> Saved Results List</ListSubheader>
+          {
+            this.props.results.map((result, index) => Result(result, index))
+          }
+        </List>
+      </Fragment>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+const mapStateToProps = (state) => ({
+  results: getResults(state),
+});
+
+export const Results = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ResultsComponent)
+
